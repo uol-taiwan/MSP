57abc8bffa0806f3c77c3bf2a1533857711ec133
Cynthia <67034552+ccy05327@users.noreply.github.com>
2022-07-09 11:11:38 +0800

    Draggable Sortable + Color

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/** The following code up to the function getDragAfterElement()
+ * is from https://youtu.be/jfYWwQrtzzY
+ */
 const draggables = document.querySelectorAll(".draggable");
 const containers = document.querySelectorAll(".container");
 
@@ -7,34 +10,61 @@ draggables.forEach((draggable) => {
   });
   draggable.addEventListener("dragend", () => {
     draggable.classList.remove("dragging");
+    // sort after each drag
+    sortModule();
   });
 });
 
 containers.forEach((container) => {
   container.addEventListener("dragover", (e) => {
     e.preventDefault();
-    const afterElement = getDragAfterElement(container, e.clientY);
+    const afterElement = getDragAfterElement(container, e.clientX);
     const draggable = document.querySelector(".dragging");
     if (afterElement == null) {
       container.appendChild(draggable);
     } else {
-        container.insertBefore(draggable, afterElement);
+      container.insertBefore(draggable, afterElement);
     }
   });
 });
 
-function getDragAfterElement(container, y) {
+function sortModule() {
+  let toSort = document.getElementById("all_modules").children;
+  toSort = Array.prototype.slice.call(toSort, 0);
+  toSort.sort((a, b) => {
+    const aord = +a.id;
+    const bord = +b.id;
+    return aord - bord < 0 ? -1 : 1;
+  });
+
+  let parent = document.getElementById("all_modules");
+  parent.innerHTML = "";
+  for (let i = 0; i < toSort.length; i++) {
+    parent.appendChild(toSort[i]);
+  }
+}
+
+function getDragAfterElement(container, x) {
   const draggableElements = [
     ...container.querySelectorAll(".draggable:not(.dragging)"),
   ];
 
-  return draggableElements.reduce((closest, child) => {
-    const box = child.getBoundingClientRect();
-    const offset = y - box.top - box.height / 2;
-    if (offset < 0 && offset > closest.offset) {
-      return { offset: offset, element: child };
-    } else {
-      return closest;
-    }
-  }, { offset: Number.POSITIVE_INFINITY }.element);
+  return draggableElements.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = x - box.right - box.width / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child };
+      } else {
+        return closest;
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element;
 }
+
+// End of cited code
+
+/** Sort Modules if dragged back
+ * https://stackoverflow.com/questions/5066925/javascript-only-sort-a-bunch-of-divs
+ */
